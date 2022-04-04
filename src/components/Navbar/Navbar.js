74e616220dc7d95d754e47ca1781efec4a3f4e3e
Your Name <you@example.com>
2022-04-04 23:25:13 +0600

    full ok done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon bg-light"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
